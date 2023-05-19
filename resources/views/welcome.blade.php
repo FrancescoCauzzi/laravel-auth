f772814dc7b66ad936e2b645ecf0554347b723e1
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-19 16:59:20 +0200

    feat: style improved more

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,22 @@
 @section('content')
 
 <div class="jumbotron p-5 bg-light rounded-3 __jumbo">
-    <div class="container">
-        <div class="logo_boolean">
-            <img src="{{Vite::asset('resources/img/boolean-logo.png')}}" alt="">
+    <div class="container d-flex">
+        <div class="__welcome">
 
+            <div class="logo_boolean">
+                <img src="{{Vite::asset('resources/img/boolean-logo.png')}}" alt="">
+
+            </div>
+            <h1 class="display-5 fw-bold">
+                Welcome to Boolfolio
+            </h1>
+
+            <p class="col-md-8 fs-4">This is a web application to manage IT projects</p>
         </div>
-        <h1 class="display-5 fw-bold">
-            Welcome to Boolfolio
-        </h1>
-
-        <p class="col-md-8 fs-4">This is a web application to manage IT projects</p>
+        <div class="__image">
+            <img src="{{Vite::asset('resources/img/jumbo-image.jpg')}}" alt="">
+        </div>
 
     </div>
 </div>
